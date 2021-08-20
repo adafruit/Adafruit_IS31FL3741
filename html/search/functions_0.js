9f8a4557d1d70636d3700f5e3c1b5ef74f2b6eca
Doxygen CI <ci-arduino@invalid>
2021-08-20 19:34:01 +0000

    Deploy docs to GitHub Pages from commit 9e2b466fd4
    Commit: 9e2b466fd4a6238ca1b90c930e2db612aefad5b4
    GitHub Actions run: 1151840708

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['adafruit_5fis31fl3741',['Adafruit_IS31FL3741',['../class_adafruit___i_s31_f_l3741.html#a0deedf0527b50c9b6a4fb75d3b5c2901',1,'Adafruit_IS31FL3741']]],
-  ['adafruit_5fis31fl3741_5fevb',['Adafruit_IS31FL3741_EVB',['../class_adafruit___i_s31_f_l3741___e_v_b.html#a3184dca913c7e8db50764c15bd5febdb',1,'Adafruit_IS31FL3741_EVB']]]
+  ['adafruit_5fis31fl3741_5fevb',['Adafruit_IS31FL3741_EVB',['../class_adafruit___i_s31_f_l3741___e_v_b.html#a3184dca913c7e8db50764c15bd5febdb',1,'Adafruit_IS31FL3741_EVB']]],
+  ['adafruit_5fis31fl3741_5fqt',['Adafruit_IS31FL3741_QT',['../class_adafruit___i_s31_f_l3741___q_t.html#a62dd028b7a6d31efa85f8bde9db67d7c',1,'Adafruit_IS31FL3741_QT']]]
 ];
