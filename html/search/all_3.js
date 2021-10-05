5ccd7e125fcd5844081041a53fb3022e5a2df7d2
Doxygen CI <ci-arduino@invalid>
2021-10-05 00:19:54 +0000

    Deploy docs to GitHub Pages from commit dc9c419d70
    Commit: dc9c419d70d4ec17080aca667ea5d402a174fab8
    GitHub Actions run: 1305785630

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['canvas',['canvas',['../class_adafruit___eye_lights___base.html#a068f4683c2a2867cdd7de717e871b788',1,'Adafruit_EyeLights_Base::canvas()'],['../class_adafruit___i_s31_f_l3741___glasses_matrix__buffered.html#accbcfcede7a0c037640e1f8c6c901162',1,'Adafruit_IS31FL3741_GlassesMatrix_buffered::canvas()']]],
+  ['color',['Color',['../class_adafruit___i_s31_f_l3741.html#a9891a74220f8569565522f192091d9d4',1,'Adafruit_IS31FL3741']]],
   ['color565',['color565',['../class_adafruit___i_s31_f_l3741.html#ad775e359fed05770985ed2ba98303084',1,'Adafruit_IS31FL3741::color565(uint8_t red, uint8_t green, uint8_t blue)'],['../class_adafruit___i_s31_f_l3741.html#ac6f40317a585f8ad3e4b701804bc1f8f',1,'Adafruit_IS31FL3741::color565(uint32_t color)']]],
   ['colorhsv',['ColorHSV',['../class_adafruit___i_s31_f_l3741.html#a4fe0030f36db85bb6f4db212081aff04',1,'Adafruit_IS31FL3741']]]
 ];
--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['gamma32',['gamma32',['../class_adafruit___i_s31_f_l3741.html#a062c968f626e49797a22960ab34c63c1',1,'Adafruit_IS31FL3741']]],
+  ['gamma8',['gamma8',['../class_adafruit___i_s31_f_l3741.html#a849f59578116287729ab16ac6499e3ab',1,'Adafruit_IS31FL3741']]],
   ['getbuffer',['getBuffer',['../class_adafruit___i_s31_f_l3741__buffered.html#a6f2fe3b0efd5d5a05fc6bfe4faa1e449',1,'Adafruit_IS31FL3741_buffered']]],
   ['getcanvas',['getCanvas',['../class_adafruit___eye_lights___base.html#a6d39cea28f1387c4b26e0561f95c7b29',1,'Adafruit_EyeLights_Base::getCanvas()'],['../class_adafruit___i_s31_f_l3741___glasses_matrix__buffered.html#a0369d84f5862aa0cb72448098dfad03e',1,'Adafruit_IS31FL3741_GlassesMatrix_buffered::getCanvas()']]],
   ['getglobalcurrent',['getGlobalCurrent',['../class_adafruit___i_s31_f_l3741.html#abe2ab3f12ff114cbe07dd8bf87916af3',1,'Adafruit_IS31FL3741']]],
